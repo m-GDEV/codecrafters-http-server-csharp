918a6081e5493e30fb7e5afe1ab135230b082381
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-05-11 21:44:16 -0400

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,37 +1,56 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+
+byte[] generatedReturnByteArray(string status, string contentType, string responseBody) {
+    // Status Line
+    string response = $"HTTP/1.1 {status}\r\n";
+    response += "\r\n";
+
+    // Headers
+    response += $"Content-Type: {contentType}\r\n";
+    response += $"Content-Length: {responseBody.Length}\r\n";
+    response += "\r\n";
+
+    // Response Body
+    response += responseBody;
+
+    return Encoding.UTF8.GetBytes(response);
+}
+
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+// Create TcpListener
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-Socket client =  server.AcceptSocket(); // wait for client
 
-Console.WriteLine("Connection Established");
+while (true) {
+    // Create new socket
+    Socket client =  server.AcceptSocket();
+    Console.WriteLine("Connection Established");
 
-byte[] request_text = new byte[100];
-client.Receive(request_text);
+    byte[] request_text = new byte[100];
+    client.Receive(request_text);
 
-string parsed = System.Text.Encoding.UTF8.GetString(request_text);
-string[] parsed_lines = parsed.Split("\r\n");
-string[] path_words = parsed_lines[0].Split(" ");
-string path = path_words[1];
+    string parsed = System.Text.Encoding.UTF8.GetString(request_text);
+    string[] parsed_lines = parsed.Split("\r\n");
+    string[] path_words = parsed_lines[0].Split(" ");
+    string path = path_words[1];
 
-Console.WriteLine($"Path is: {path}");
+    Console.WriteLine($"Path is: {path}");
 
-if (String.Equals("/", path)) {
-    string return_string = "HTTP/1.1 200 OK\r\n\r\n";
-    byte[] return_array = System.Text.Encoding.ASCII.GetBytes(return_string);
+    if (path.StartsWith("/echo")) {
+        string[] words = path.Split("/");
+        client.Send(generatedReturnByteArray("200 OK", "text/plain", words[1]));
+    }
+    else {
+        client.Send(generatedReturnByteArray("404 Not Found", "text/plain", "Nothing Dipshit"));
+    }
 
-    client.Send(return_array);
-}
-else {
-    string return_string = "HTTP/1.1 404 Not Found\r\n\r\n";
-    byte[] return_array = System.Text.Encoding.ASCII.GetBytes(return_string);
+    client.Close();
 
-    client.Send(return_array);
 }
 
 
